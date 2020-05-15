0fc2b9e7b494ab972a94a2c9877ebea199405f33
Lam Pham <phamlamuet@gmail.com>
2020-05-15 22:35:09 +0700

    add a star

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
-
-#include<vector>
 #include <iostream>
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include<cstdlib>
 #include<ctime>
+#include<algorithm>
+#include<list>
 using namespace std;
 
+const int scale=24;
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 577;
-const int scale=24;
+const int nMapWidth=24;
+const int nMapHeight=24;
 
 enum Status
 {
@@ -21,17 +23,25 @@ enum Status
 };
 
 const string WINDOW_TITLE = "Snake game";
-
 void initSDL(SDL_Window* &window, SDL_Renderer* &renderer);
 void logSDLError(std::ostream& os, //SOME PREBAKED FUNCTION
                  const std::string &msg, bool fatal = false);
 void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
 void waitUntilKeyPressed();
 
-//code here
-
 struct ToaDo
 {
+    int x;
+    int y;
+};
+struct sNode
+{
+    bool bObstacle;
+    bool bVisited;
+    float fGlobalGoal;
+    float fLocalGoal;
+    vector<sNode*>vecNeighbours;
+    sNode*parent;
     int x;
     int y;
 };
@@ -106,6 +116,7 @@ bool isFoodOnSnake(ToaDo &food,Snake &snake)
     }
     return valid;
 }
+
 ToaDo spawnFood(Snake &snake)
 {
     srand(time(0));
@@ -121,7 +132,6 @@ ToaDo spawnFood(Snake &snake)
 
     return foodTemp;
 }
-
 void screenRender(SDL_Renderer *renderer)
 {
     SDL_SetRenderDrawColor(renderer,255,255,0,255);
@@ -129,7 +139,6 @@ void screenRender(SDL_Renderer *renderer)
     SDL_RenderDrawLine(renderer, 0, scale*scale, scale*scale, scale*scale);
     SDL_RenderDrawLine(renderer, scale*scale, scale*scale, scale*scale, 0);
     SDL_RenderDrawLine(renderer, scale*scale, 0, 0, 0);
-
 
     for(int i=1; i<scale; i++)
     {
@@ -139,9 +148,41 @@ void screenRender(SDL_Renderer *renderer)
     {
         SDL_RenderDrawLine(renderer, i*scale,0, i*scale, scale*scale);
     }
-
 }
-bool gameOver(Snake &snake)
+void pathRender(SDL_Renderer *renderer,vector<sNode*>path)
+{
+    SDL_SetRenderDrawColor(renderer,0,0,255,255);
+    for(int i=1;i<path.size();i++)
+    {
+        SDL_Rect pathRect;
+        pathRect.x=path[i]->x;
+        pathRect.y=path[i]->y;
+        pathRect.h=scale;
+        pathRect.w=scale;
+        SDL_RenderFillRect(renderer,&pathRect);
+    }
+}
+/*void visitedBlockRender(SDL_Renderer *renderer,sNode*nodes)
+{
+   SDL_SetRenderDrawColor(renderer,0,100,100,255);
+
+   for(int i=0; i<nMapWidth; i++)
+    {
+        for(int j=0; j<nMapHeight; j++)
+        {
+            if(nodes[j*nMapWidth + i].bVisited ==true)
+            {
+                SDL_Rect visitedBlockRect;
+                visitedBlockRect.x=nodes[j*nMapWidth+i].x;
+                visitedBlockRect.y=nodes[j*nMapWidth+i].y;
+                visitedBlockRect.h=scale;
+                visitedBlockRect.w=scale;
+                SDL_RenderFillRect(renderer,&visitedBlockRect);
+            }
+        }
+    }
+}   */
+bool gameOver(Snake snake)
 {
     if(snake.head.x<0||snake.head.y<0||snake.head.x>scale*(scale-1)||snake.head.y>scale*(scale-1))
     {
@@ -156,28 +197,190 @@ bool gameOver(Snake &snake)
     }
     return false;
 }
+
+Snake snakeInit(Snake &snake,ToaDo &preHead)
+{
+    snake.tailLength=0;
+    snake.head.x=0;
+    snake.head.y=0;
+    preHead.x=0;
+    preHead.y=0;
+    snake.status=STOP;
+
+    return snake;
+}
+
+vector<sNode*> solveAstar(sNode*nodeStart,sNode*nodeEnd,sNode*nodes,Snake &snake)
+{
+    vector<sNode*>path;
+    for(int x=0; x<nMapWidth; x++)
+    {
+        for(int y=0; y<nMapHeight; y++)
+        {
+            nodes[y*nMapWidth+x].fGlobalGoal=INFINITY;
+            nodes[y*nMapWidth+x].fLocalGoal=INFINITY;
+            nodes[y*nMapWidth + x].bVisited = false;
+            nodes[y*nMapWidth + x].parent = nullptr;
+            nodes[y*nMapWidth+x].bObstacle=false;
+        }
+    }
+    for(int i=0; i<snake.tail.size(); i++)
+    {
+        nodes[snake.tail[i].x/24+snake.tail[i].y].bObstacle=1;
+       // cout<<snake.tail[i].x/24<<","<<snake.tail[i].y/24;
+       // cout<<"is obstacle"<<endl;
+    }
+   // nodes[snake.head.x/24+snake.head.y].bObstacle=1;
+    auto distance=[](sNode*a,sNode*b)
+    {
+        //return max(fabs(a->x-b->x),fabs(a->y-b->y));
+        return fabs((a->x-b->x))+fabs((a->y-b->y));
+       // return sqrt((a->x-b->x)*(a->x-b->x)+(a->y-b->y)*(a->y-b->y));
+    };
+    auto heuristic=[distance](sNode*a,sNode*b)
+    {
+        return distance(a,b);
+    };
+    sNode *nodeCurrent=nodeStart;
+    nodeStart->fLocalGoal=0.0f;
+    nodeStart->fGlobalGoal=heuristic(nodeStart,nodeEnd);
+    /** list of nodes to be tested ->> openlist */
+    list<sNode*> listNotTestedNodes;
+    listNotTestedNodes.push_back(nodeStart); /** startNode is the first to be tested*/
+    while (!listNotTestedNodes.empty() && nodeCurrent != nodeEnd  )// Find absolutely shortest path // && nodeCurrent != nodeEnd)
+    {
+        // Sort Untested nodes by global goal, so lowest is first
+        listNotTestedNodes.sort([](const sNode* lhs, const sNode* rhs)
+        {
+            return lhs->fGlobalGoal < rhs->fGlobalGoal;
+        } );
+
+        // Front of listNotTestedNodes is potentially the lowest distance node. Our
+        // list may also contain nodes that have been visited, so ditch these...
+        while(!listNotTestedNodes.empty() && listNotTestedNodes.front()->bVisited)
+            listNotTestedNodes.pop_front();
+
+        // ...or abort because there are no valid nodes left to test
+        if (listNotTestedNodes.empty())
+            break;
+
+        nodeCurrent = listNotTestedNodes.front();
+        nodeCurrent->bVisited = true; // We only explore a node once
+
+        // Check each of this node's neighbours...
+        for (auto nodeNeighbour : nodeCurrent->vecNeighbours)
+        {
+            // ... and only if the neighbour is not visited and is
+            // not an obstacle, add it to NotTested List
+            if (!nodeNeighbour->bVisited && nodeNeighbour->bObstacle == 0)
+                listNotTestedNodes.push_back(nodeNeighbour);
+
+            // Calculate the neighbours potential lowest parent distance
+            float fPossiblyLowerGoal = nodeCurrent->fLocalGoal + distance(nodeCurrent, nodeNeighbour);
+
+            // If choosing to path through this node is a lower distance than what
+            // the neighbour currently has set, update the neighbour to use this node
+            // as the path source, and set its distance scores as necessary
+            if (fPossiblyLowerGoal < nodeNeighbour->fLocalGoal)
+            {
+                nodeNeighbour->parent = nodeCurrent;
+                nodeNeighbour->fLocalGoal = fPossiblyLowerGoal;
+                nodeNeighbour->fGlobalGoal = nodeNeighbour->fLocalGoal + heuristic(nodeNeighbour, nodeEnd);
+            }
+        }
+    }
+    if(nodeEnd!=nullptr)
+    {
+        sNode *p=nodeEnd;
+        while(p->parent!=nullptr)
+        {
+            path.push_back(p);
+            p=p->parent;
+        }
+    }
+  /*  for(int i=path.size()-1;i>=0;i--)
+    {
+        cout<<path[i]->x/24<<","<<path[i]->y/24<<endl;
+    }  */
+    return path;
+}
+void getPath(vector<sNode*>path,Snake&snake)
+{
+    int pathSize=path.size();
+    if(pathSize==0)
+    {
+        return;
+    }
+    else if(path[pathSize-1]->x==snake.head.x&&path[pathSize-1]->y==snake.head.y)
+    {
+          return;
+    }
+    else if(path[pathSize-1]->x==snake.head.x&&path[pathSize-1]->y<snake.head.y&&snake.status!=DOWN)
+    {
+        snake.status=UP;
+
+    }
+     else if(path[pathSize-1]->y==snake.head.y&&path[pathSize-1]->x<snake.head.x&&snake.status!=RIGHT)
+     {
+         snake.status=LEFT;
+
+     }
+     else if(path[pathSize-1]->y==snake.head.y&&path[pathSize-1]->x>snake.head.x&&snake.status!=LEFT)
+     {
+         snake.status=RIGHT;
+     }
+     else if(path[pathSize-1]->x==snake.head.x&&path[pathSize-1]->y>snake.head.y&&snake.status!=UP)
+     {
+         snake.status=DOWN;
+
+     }
+}
+void getPath2(vector<sNode*>path,Snake&snake)
+{
+    int pathSize=path.size();
+    if(pathSize==0)
+    {
+        return;
+    }
+    else if(path[pathSize-1]->x==snake.head.x&&path[pathSize-1]->y==snake.head.y)
+    {
+          return;
+    }
+    else if(path[pathSize-1]->y>snake.head.y)
+    {
+        snake.status=DOWN;
+    }
+    else if(path[pathSize-1]->y<snake.head.y)
+    {
+        snake.status=UP;
+    }
+
+    else if(path[pathSize-1]->x>snake.head.x)
+    {
+        snake.status=RIGHT;
+    }
+    else if(path[pathSize-1]->x<snake.head.x)
+    {
+        snake.status=LEFT;
+    }
+}
+
 int main(int argc, char* argv[])
 {
-    cout<<"debug1";
-    // SDL_Init(SDL_INIT_EVERYTHING);
+    cout<<"Just Do It";
+    srand(time(0));
     TTF_Init();
     SDL_Window* window;
     SDL_Renderer* renderer;
 
     SDL_Event event;
     initSDL(window, renderer);
-  //  SDL_RenderClear(renderer);
+
     SDL_SetRenderDrawColor(renderer,255,0,0,255);
-    //screenRender(renderer);
 
     Snake snake;
-    snake.tailLength=0;
-    snake.head.x=0;
-    snake.head.y=0;
     ToaDo preHead;
-    preHead.x=0;
-    preHead.y=0;
-
+    snakeInit(snake,preHead);
 
     SDL_Rect snakeRect;
     snakeRect.x=0;
@@ -185,25 +388,58 @@ int main(int argc, char* argv[])
     snakeRect.h=0;
     snakeRect.w=0;
 
-    // snakeRender(renderer,snakeRect,snake);
-
     ToaDo food;
     food=spawnFood(snake);
     SDL_Rect foodRect;
 
-//   foodRender(renderer,foodRect,food);
+    sNode *nodes=new sNode[nMapWidth*nMapHeight]; /** array of nodes */
+
+    for(int x=0; x<nMapWidth; x++)
+    {
+        for(int y=0; y<nMapHeight; y++)
+        {
+            nodes[y * nMapWidth + x].x = x*scale;
+            nodes[y * nMapWidth + x].y = y*scale;
+            nodes[y * nMapWidth + x].bObstacle = false;
+            nodes[y * nMapWidth + x].parent = nullptr;
+            nodes[y * nMapWidth + x].bVisited = false;
+        }
+    }
+    sNode*nodeStart=&nodes[3];  /**start node */
+    sNode*nodeEnd=&nodes[8];   /**end node */
 
 
-    // SDL_RenderPresent(renderer);
-    // bool inputThisFrame=false;
+    /**make connection */
+    for(int x=0; x<nMapWidth; x++)
+    {
+        for(int y=0; y<nMapHeight; y++)
+        {
+            if(y>0)
+                nodes[y*nMapWidth + x].vecNeighbours.push_back(&nodes[(y - 1) * nMapWidth + (x + 0)]);
+            if(y<nMapHeight-1)
+                nodes[y*nMapWidth + x].vecNeighbours.push_back(&nodes[(y + 1) * nMapWidth + (x + 0)]);
+            if (x>0)
+                nodes[y*nMapWidth + x].vecNeighbours.push_back(&nodes[(y + 0) * nMapWidth + (x - 1)]);
+            if(x<nMapWidth-1)
+                nodes[y*nMapWidth + x].vecNeighbours.push_back(&nodes[(y + 0) * nMapWidth + (x + 1)]);
+        }
+    }
+    nodeStart=&nodes[snake.head.x/24+snake.head.y];
+    nodeEnd=&nodes[food.x/24+food.y];
 
-    float time=SDL_GetTicks()/100;
+    cout<<"test ok ok"<<endl;
+    nodeStart=&nodes[snake.head.x/24+snake.head.y];
+    nodeEnd=&nodes[food.x/24+food.y];
+    vector<sNode*>path;
+    path=solveAstar(nodeStart,nodeEnd,nodes,snake);
+
     while(true)
     {
-        float newTime=SDL_GetTicks()/75; // thoi gian block duoc update
-        float delta=newTime-time;
-        time=newTime;
-
+        // cout<<nodeStart->x<<" "<<snake.head.x<<endl;
+        //SDL_Delay(100);
+        preHead.x=snake.head.x;
+        preHead.y=snake.head.y;
+        // cout<<snake.head.x/24<<" "<<snake.head.y/24<<" "<<snake.status<<endl;
         if(SDL_PollEvent(&event))
         {
             if(event.type==SDL_QUIT)
@@ -213,29 +449,36 @@ int main(int argc, char* argv[])
 
                 if (snake.status!=DOWN && event.key.keysym.scancode == SDL_SCANCODE_UP)
                 {
-                    snake.status=UP;                //CONTROL
-
+                    snake.status=UP;
                 }
                 else if (snake.status != RIGHT && event.key.keysym.scancode == SDL_SCANCODE_LEFT)
                 {
                     snake.status=LEFT;
-
                 }
                 else if (snake.status !=UP && event.key.keysym.scancode == SDL_SCANCODE_DOWN)
                 {
                     snake.status=DOWN;
-
                 }
                 else if (snake.status!=LEFT && event.key.keysym.scancode == SDL_SCANCODE_RIGHT)
                 {
                     snake.status=RIGHT;
-
                 }
-
             }
         }
-        preHead.x=snake.head.x;
-        preHead.y=snake.head.y;
+        else
+        {
+            //path.clear();
+            //std::vector<sNode*>().swap(path);
+           // nodeStart=&nodes[snake.head.x/24+snake.head.y];
+           // nodeEnd=&nodes[food.x/24+food.y];
+            //path=solveAstar(nodeStart,nodeEnd,nodes,snake);
+
+            getPath2(path,snake);
+            //tracePath(path,snake);
+            path.pop_back();
+
+        }
+
         Snake snakeTemp;
         snakeTemp.head=preHead;
         snakeTemp.tailLength=snake.tailLength;
@@ -246,44 +489,47 @@ int main(int argc, char* argv[])
 
         if(snake.status==UP)
         {
-            snake.head.y-=delta*scale;
+            snake.head.y-=scale;
         }
         else if(snake.status==LEFT)
         {
-            snake.head.x-=delta*scale;
+            snake.head.x-=scale;
         }
         else if(snake.status==RIGHT)
         {
-            snake.head.x+=delta*scale;
+            snake.head.x+=scale;
         }
         else if(snake.status==DOWN)
         {
-            snake.head.y+=delta*scale;
+            snake.head.y+=scale;
         }
+
         if(food.x==snake.head.x&&food.y==snake.head.y)
         {
-            food= spawnFood(snake);
+
             snake.tailLength++;
+            snake.tail.push_back(preHead);
+            food= spawnFood(snake);
+            path.clear();
+            std::vector<sNode*>().swap(path);
+            SDL_Delay(100);
+            nodeStart=&nodes[snake.head.x/24+snake.head.y];
+            nodeEnd=&nodes[food.x/24+food.y];
+            path=solveAstar(nodeStart,nodeEnd,nodes,snake);
+        //    cout<<"path size is"<<path.size()<<endl;
+        //   cout<<"start node is "<<nodeStart->x/24<<","<<nodeStart->y/24<<endl;
+        //   cout<<"node end is "<<nodeEnd->x/24<<","<<nodeEnd->y/24<<endl;
         }
-        if(delta==1)
+        for(int i=1; i<snake.tailLength; i++)
         {
-            if(snake.tail.size()!=snake.tailLength)
-            {
-                snake.tail.push_back(preHead);
-            }
-            for(int i=0; i<snake.tailLength; i++)
-            {
-                if(i>0)
-                {
-                    snake.tail[i-1].x=snake.tail[i].x;
-                    snake.tail[i-1].y=snake.tail[i].y;
-                }
-            }
-            if(snake.tailLength>0)
-            {
-                snake.tail[snake.tailLength-1].x=preHead.x;
-                snake.tail[snake.tailLength-1].y=preHead.y;
-            }
+            snake.tail[i-1].x=snake.tail[i].x;
+            snake.tail[i-1].y=snake.tail[i].y;
+
+        }
+        if(snake.tailLength>0)
+        {
+            snake.tail[snake.tailLength-1].x=preHead.x;
+            snake.tail[snake.tailLength-1].y=preHead.y;
         }
 
         if(gameOver(snake))
@@ -297,21 +543,14 @@ int main(int argc, char* argv[])
         }
         foodRender(renderer,foodRect,food);
         screenRender(renderer);
+
+        pathRender(renderer,path);
         scoreRender(renderer,snake);
+        //visitedBlockRender(renderer,nodes);
         SDL_RenderPresent(renderer);
         SDL_SetRenderDrawColor(renderer, 105, 105, 105, 255);
         SDL_RenderClear(renderer);
     }
-
-
-
-
-
-    // SDL_RenderPresent(renderer);
-
-    // Your drawing code here
-    // use SDL_RenderPresent(renderer) to show it
-
     waitUntilKeyPressed();
     quitSDL(window, renderer);
     return 0;
@@ -367,5 +606,3 @@ void waitUntilKeyPressed()
         SDL_Delay(100);
     }
 }
-
-
